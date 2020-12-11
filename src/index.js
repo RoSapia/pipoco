376184a1ae3e74c6effb59ea5ab05d2b6f627829
RoSapia <rosapia@DESKTOP-BG3EG6H.localdomain>
2020-12-11 10:01:25 -0300

    limpando página para receber novo conteudo

--- src/index.js
+++ src/index.js
@@ -5,7 +5,22 @@ import contacts from "./pages/contacts/index.js"
 const main = document.querySelector("root")
 
 const init = () =>{
-    window.addEventListener("hashchange", () => console.log(window.location.hash))
+    window.addEventListener("hashchange", () => {
+        main.innerHTML = ""        
+        switch(window.location.hash){
+            case " ":
+                main.appendChild(home())
+                break
+            case "#about":
+                main.appendChild(about())
+                break
+            case "#contacts":
+                main.appendChild(contacts())
+                break
+            default:
+                main.appendChild(home())
+        }
+    })
 }
 
 window.addEventListener("load", () =>{
