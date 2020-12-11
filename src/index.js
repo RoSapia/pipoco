4c7232d34220c09347a2a96b0079f3db8dcee9f6
RoSapia <rosapia@DESKTOP-BG3EG6H.localdomain>
2020-12-11 09:53:00 -0300

    adicionando paginas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import home from "./pages/home/index.js"
+import about from "./pages/about/index.js"
+import contacts from "./pages/contacts/index.js"
 
 const main = document.querySelector("root")
 
+const init = () =>{
+    window.addEventListener("hashchange", () => console.log(window.location.hash))
+}
+
 window.addEventListener("load", () =>{
     main.appendChild(home())
+    init()
 } )
